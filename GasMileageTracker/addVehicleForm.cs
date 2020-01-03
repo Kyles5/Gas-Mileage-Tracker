0accaa9cc750955b7bf659b4bd0ac247e0972d60
Kyle Scheider <kyle.scheider78@gmail.com>
2020-01-03 10:47:33 -0500

    Add project files.

---
+++ GasMileageTracker/addVehicleForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GasMileageTracker
+{
+    public partial class addVehicleForm : Form
+    {
+        public addVehicleForm()
+        {
+            InitializeComponent();
+        }
+
+        private void add(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
